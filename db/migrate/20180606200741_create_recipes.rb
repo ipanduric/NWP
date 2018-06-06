cf0e63b42923dee8b51469d95e2192916ccee9b4
ipanduric <ipanduric1@gmail.com>
2018-06-06 20:16:22 +0000

    Dropping the database and creating the new one

--- db/migrate/20180606200741_create_recipes.rb
+++ db/migrate/20180606200741_create_recipes.rb
@@ -7,4 +7,3 @@ class CreateRecipes < ActiveRecord::Migration[5.0]
     end
   end
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606192815) do
+ActiveRecord::Schema.define(version: 20180606201124) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -21,7 +21,11 @@ ActiveRecord::Schema.define(version: 20180606192815) do
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.integer "chef_id"
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "chef_id"
   end
 
 end
